5eb5def7ea487d9668e17270a25900b277c41d4b
Ken Yasue <ken.yasue@clover-studio.com>
2015-11-03 17:07:04 +0100

    remove hard coded url

--- src/server/lib/init.js
+++ src/server/lib/init.js
@@ -9,9 +9,9 @@
     Config.urlPrefix = '/api/v1';
 
     Config.dbCollectionPrefix = '';
-    Config.databaseUrl = "mongodb://localhost/user";
-    Config.publicPath = "/Users/kenyasue/Documents/Projects/FBMessengerClone/public/";
-    Config.uploadPath = "/Users/kenyasue/Documents/Projects/FBMessengerClone/public/uploads/";
+    Config.databaseUrl = "mongodb://localhost/simplemessenger";
+    Config.publicPath = "/Users/kenyasue/Documents/Projects/SimpleMessenger/public/";
+    Config.uploadPath = "/Users/kenyasue/Documents/Projects/SimpleMessenger/public/uploads/";
 
     Config.emailService = 'Gmail';
     Config.emailFrom = '';
