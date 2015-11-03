d48baab6b7d7b0059d713c92b9378b3c13ea2d95
Ken Yasue <ken.yasue@clover-studio.com>
2015-11-03 16:08:05 +0000

    Save init

--- src/server/lib/init.js
+++ src/server/lib/init.js
@@ -5,13 +5,13 @@
     var Config = {};
 
     Config.host = "localhost";
-    Config.port = 8080;
+    Config.port = 80;
     Config.urlPrefix = '/api/v1';
 
     Config.dbCollectionPrefix = '';
     Config.databaseUrl = "mongodb://localhost/user";
-    Config.publicPath = "/Users/kenyasue/Documents/Projects/FBMessengerClone/public/";
-    Config.uploadPath = "/Users/kenyasue/Documents/Projects/FBMessengerClone/public/uploads/";
+    Config.publicPath = "/home/admin/server/SimpleMesssenger/public/";
+    Config.uploadPath = "/home/admin/server/SimpleMesssenger/public/uploads/";
 
     Config.emailService = 'Gmail';
     Config.emailFrom = '';
