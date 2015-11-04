147a66429a4032d1695a6af46c333aac16d8d329
Ken Yasue <ken.yasue@clover-studio.com>
2015-11-04 16:06:22 +0000

    save changes

--- doc/API/api_project.js
+++ doc/API/api_project.js
@@ -1,1 +1,15 @@
-define({  "name": "SimpleMessenter",  "version": "0.1.0",  "description": "Simple Messenger API Document",  "title": "Simple Messenger",  "url": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-11-04T14:33:03.671Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
+define({
+  "name": "SimpleMessenter",
+  "version": "0.1.0",
+  "description": "Simple Messenger API Document",
+  "title": "Simple Messenger",
+  "url": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-11-04T16:05:31.834Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
